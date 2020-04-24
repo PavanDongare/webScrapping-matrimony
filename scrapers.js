b9741f75c41f61cfd889cdbaa84d3c32506d8baf
pavan <dongare.pavan25@gmail.com>
2020-04-25 02:41:01 +0530

    id on a page

--- scrapers.js
+++ scrapers.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-const download = require('image-downloader')
 const url = 'https://www.anandmaratha.com/maratha-brides/';
 const id=[];
 
@@ -9,7 +8,7 @@ async function run () {
     var id=[];
 
     var i;
-    for (i = 1; i < 1 ; i++) {
+    for (i = 1; i < 2 ; i++) {
         const page = await browser.newPage();        
         await page.goto(url+i.toString());
         let texts = await page.evaluate(() => {
@@ -25,33 +24,9 @@ async function run () {
 
     console.log(id.length);
 
-    for(i=0;i<id.length;i++)
-    {
-        var url = 'https://www.anandmaratha.com/girls/'+id[i]+'jpg';
-        
-    }
-
     browser.close();
 }
 
 
 run();
 
-
-
-
-const options = {
-    url: 'https://www.anandmaratha.com/girls/mg108978.jpg',
-    dest: '/Users/pavan/Desktop/scrapping/girls'
-  }
-   
-async function downloadIMG(options) {
-    try {
-      const { filename, image } = await download.image(options)
-      console.log(filename) // => /path/to/dest/image.jpg
-    } catch (e) {
-      console.error(e)
-    }
-}
-   
-  downloadIMG(options);
