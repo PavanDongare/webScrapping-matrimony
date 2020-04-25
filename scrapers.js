a9f21a5561a36aa5d33ba71860dc0e432537273d
pavan <dongare.pavan25@gmail.com>
2020-04-25 21:05:45 +0530

    checkpt

--- scrapers.js
+++ scrapers.js
@@ -10,16 +10,14 @@ async function run () {
     var id=[];
 
     var i;
-    for (i = 1; i < 2 ; i++) {
+    for (i = 1; i < 4 ; i++) {
         const page = await browser.newPage();        
         await page.goto(urlBase+i.toString());
         let texts = await page.evaluate(() => {
             let data = [];
             let elements = document.getElementsByClassName('list-td');
-            // var href =     document.getElementsByClassName("list-td")[0].href;
-
             for (var element of elements)
-                data.push('https://www.anandmaratha.com/girls/'+element.textContent.toLowerCase()+'.jpg');
+                data.push('https://www.anandmaratha.com/girls/'+element.textContent.toLowerCase()+'.jpg'+' '+element.href);
             return data;
         });
 
@@ -28,29 +26,8 @@ async function run () {
 
     console.log(id);
 
-    for(i=0;i<id.length;i++)
-    {
-        var urlImage = 'https://www.anandmaratha.com/girls/'+id[i].toLowerCase()+'.jpg';
-        const options = {
-            url: urlImage,
-            dest: '/Users/pavan/Desktop/scrapping/girls'
-        }
-       // downloadIMG(options);
-        // console.log(options);
-    }
-
     browser.close();
 }
-
-async function downloadIMG(options) {
-    try {
-      const { filename, image } = await download.image(options)
-      console.log(filename) // => /path/to/dest/image.jpg
-    } catch (e) {
-      console.error(e)
-    }
-}
-   
 
 run();
 
